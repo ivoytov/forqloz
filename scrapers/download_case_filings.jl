e3c4636a79a5cd239bd96d7c3c01a35cc6d7449d
Ilya Voytov <ilya@voytov.com>
2026-09-20 21:32:44 -0400

    fix: hanging bug due to old puppeteer version

--- scrapers/download_case_filings.jl
+++ scrapers/download_case_filings.jl
@@ -93,6 +93,9 @@ end
 
 # Function to find missing filings
 function missing_filings(case_number, auction_date)
+    if auction_date !== missing && auction_date < today() - Day(60)
+        return []
+    end
     res = []
     for (key, dir) in FilingType
         filename = expected_filename(dir, case_number, auction_date)
